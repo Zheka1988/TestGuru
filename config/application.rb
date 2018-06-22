f4d2b1f27c733f19685fadac159299a3b1a62cda
Zheka1988 <kveladze.kz@mail.ru>
2018-06-22 03:07:44 -0700

    first attempt

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,18 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    attr_reader :version
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = "Astana"
+    config.active_record.default_timezone = :local
+
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ru
+    config.i18n.fallbacks = {'ru' => 'en'}
+
+    config.before_initialize { @version = '0.0.0' }
   end
 end

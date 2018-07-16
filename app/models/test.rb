f5813302b198c1d45764e536583209d750e81b6e
Zheka1988 <kveladze.kz@mail.ru>
2018-07-16 02:12:55 -0700

    sdkjCuyf

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   has_many :traveled_tests
+
   def self.by_category(category)
     category_list = []
     category_list << Category.where('title = ?', category)

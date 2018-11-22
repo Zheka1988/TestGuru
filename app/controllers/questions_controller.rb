5b04a891a9b77d0d9967889d95a28a240b7fd198
Zheka1988 <kveladze.kz@mail.ru>
2018-11-22 01:24:52 -0800

    first popytka

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,34 +1,45 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:index, :create]
-  before_action :find_question, only: [:show, :destroy]
+  before_action :find_test, only: [:new, :create]
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :resque_question_not_found
 
-  def index
-    render plain: @test.questions.pluck(:body, :id)
-  end
-
   def show
-    render plain: @question.body
+
   end
 
   def new
-
+    @question = Question.new
   end
 
   def create
     @question = @test.questions.create(question_params)
-    @save_id = nil
-    render plain: "Question was created"
+    if @question.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @test
+    else
+      render :edit
+    end
   end
 
   def destroy
     @question.destroy
-    render plain: "Question was deleted"
+    redirect_to @test
   end
 
-
   private
+
   def question_params
     params.require(:question).permit(:body)
   end
@@ -39,10 +50,12 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = Question.find(params[:id])
+    @test = Test.find(@question.test_id)
   end
 
   def resque_question_not_found
     render plain: "Question was not found"
   end
+
 
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,17 @@
 module QuestionsHelper
+  def question_header_edit
+    render plain: "Edit #{@test.title} Question"
+  end
+
+  def question_header_create
+    render plain: "Create New #{@test.title} Question"
+  end
+
+  def current_year
+    render plain: " #{Time.current.year} year"
+  end
+
+  def github_url(author, repo)
+    render plain: "https://github.com/#{author}/#{repo}"
+  end
 end

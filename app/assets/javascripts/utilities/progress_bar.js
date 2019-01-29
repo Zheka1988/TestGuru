73394f493ac59cd14123932863d6ecfef6b7996e
Zheka1988 <kveladze.kz@mail.ru>
2019-01-28 22:42:19 -0800

    added js progress bar

---
+++ app/assets/javascripts/utilities/progress_bar.js
@@ -0,0 +1,15 @@
+document.addEventListener('turbolinks:load', function() {
+  var progressBar = document.querySelector('.progress-bar')
+
+  if (progressBar) {
+    var questionNumber = progressBar.getAttribute('value')
+    var questionsCount = progressBar.getAttribute('max')
+
+    var percent = ((questionNumber * 100) / questionsCount).toFixed(1) + '%'
+    progressBar.style.width = percent;
+    progressBar.innerHTML = percent;
+    // alert('hello')
+  }
+
+})
+

20c8d7e0416dab0be6b7b0168ea4e8a49171cdd4
Zheka1988 <kveladze.kz@mail.ru>
2019-01-30 04:53:13 -0800

    setting smtp for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,18 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(harmony: true)
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options= { host: 'gentle-earth-28935.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['SMTP_USERNAME'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

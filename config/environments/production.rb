599e29ce9e6f5104dbf40a978e26ec1fa783b0e9
Zheka1988 <kveladze.kz@mail.ru>
2019-01-30 19:18:53 -0800

    change settings SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(harmony: true)
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options= { host: 'gentle-earth-28935.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'gentle-earth-28935.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',

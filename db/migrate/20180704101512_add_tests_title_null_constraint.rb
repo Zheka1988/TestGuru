f47e90ceb37b87231628afef9449f2b1e2a94384
Zheka1988 <kveladze.kz@mail.ru>
2019-01-30 19:53:09 -0800

    delete null value column name

--- db/migrate/20180704101512_add_tests_title_null_constraint.rb
+++ db/migrate/20180704101512_add_tests_title_null_constraint.rb
@@ -1,7 +1,6 @@
 class AddTestsTitleNullConstraint < ActiveRecord::Migration[5.2]
   def change
     change_column_null(:users, :password, false)
-    change_column_null(:users, :name, false)
     change_column_null(:categories, :title, false)
     change_column_null(:tests, :title, false)
   end

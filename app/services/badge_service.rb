963615164c196c10d5afa5fbda0e740adc845f32
Zheka1988 <kveladze.kz@mail.ru>
2019-02-06 22:24:55 -0800

    снесены поправки по замечаниям 1

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -3,84 +3,58 @@ class BadgeService
     @test_passage = test_passage
     @user = test_passage.user
     @test = test_passage.test
-    @new_badges = []
   end
 
   def call
-    return [] unless @test_passage.successful_test?
-    Badge.all.each do |badge|
+    return [] unless @test_passage.successfully?
+    Badge.all.select do |badge|
       send badge.rule, badge
     end
   end
 
-
-
   private
 
-  def rewards(badge)
-    @user.badges << badge
-    @new_badges << badge
-  end
-
-  def successful_test?
-    @test_passage.successfully?
-  end
+  # def successful_test?
+  #   @test_passage.successfully?
+  # end
 
   # за прохождение всех тестов определенной категории
 
   def category_complete(badge)
-    if category_exist?(badge)
+    category = Category.find_by(title: badge.parametr)
+    if category
       cat_id = Category.find_by(title: badge.parametr).id
       tests_exist(badge, cat_id)
     end
   end
 
-  def category_exist?(badge)
-    Category.find_by(title: badge.parametr)
-  end
 
   def tests_exist(badge, cat_id)
-    if tests_exist?(cat_id)
-      rewards(badge) if all_tests_passed?(cat_id)
+    test = Test.find_by(category_id: cat_id)
+    if test
+      badge if all_tests_passed?(cat_id)
     end
   end
 
-  def tests_exist?(cat_id)
-    Test.find_by(category_id: cat_id)
-  end
-
   def all_tests_passed?(cat_id)
-    title_exist_test = []
-    Test.where(category_id: cat_id).each {|test| title_exist_test << test.title}
-
-    title_passed_test = []
-    @user.tests.where(category_id: cat_id).each {|test| title_passed_test << test.title}
-    title_passed_test.uniq!
-
-    (title_exist_test - title_passed_test).empty?
-    #title_exist_test.compare(title_passed_test) - почему то выходила ошибка
+    Test.where(category_id: cat_id).count == @user.tests.where(category_id: cat_id).distinct.count
   end
+# --------------------------------------------------------------
+
 
   # за прохождение теста с первой попытки
   def first_try(badge)
-    rewards(badge) if @user.tests.where(id: @test.id).count == 1
+    badge if @user.tests.where(id: @test.id).count == 1
   end
 
   # за прохождение всех тестов определенного уровня
   def level_complete(badge)
     if Test.find_by(level: badge.parametr.to_i)
-      rewards(badge) if all_tests_on_level?(badge)
+      badge if all_tests_on_level?(badge)
     end
   end
 
   def all_tests_on_level?(badge)
-    title_exist_test = []
-    Test.where(level: badge.parametr.to_i).each {|test| title_exist_test << test.title}
-
-    title_passed_test = []
-    @user.tests.where(level: badge.parametr.to_i).each {|test| title_passed_test << test.title}
-    title_passed_test.uniq!
-
-    (title_exist_test - title_passed_test).empty?
+    Test.where(level: badge.parametr.to_i).count ==@user.tests.where(level: badge.parametr.to_i).distinct.count
   end
 end
